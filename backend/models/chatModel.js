af25cc36e7f58481a168c9cce4a9bc35d9544947
Kevin Feng <kevinfeng.cs88@gmail.com>
2022-03-04 11:36:48 -0500

    fixed backend Router object bug

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -13,7 +13,7 @@ const chatModel = mongoose.Schema(
             ref: 'Message'
         },
         groupAdmin: {
-            type: mongoose.SChema.Types.ObjectId,
+            type: mongoose.Schema.Types.ObjectId,
             ref: 'User'
         }
     },
