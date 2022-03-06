00fc6f2077264f4ec22e6b53c8b9978964a6679a
Kevin Feng <kevinfeng.cs88@gmail.com>
2022-03-05 21:12:15 -0500

    added renameGroup functionality

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -98,4 +98,26 @@ const createGroupChat = expressAsyncHandler(async(req, res) => {
     }
 });
 
-module.exports = { accessChat, fetchChats, createGroupChat };
+const renameGroup = expressAsyncHandler(async(req, res) => {
+    const {chatId, chatName} = req.body;
+
+    const updatedChat = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            chatName
+        },{
+            new: true
+        }
+    ).populate('users', '-password')
+    .populate('groupAdmin', '-password');
+
+    if (!updatedChat){
+        res.status(404);
+        throw new Error('Chat not found');
+    }
+    else{
+        res.json(updatedChat);
+    }
+});
+
+module.exports = { accessChat, fetchChats, createGroupChat, renameGroup };
