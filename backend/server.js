5d5fdb368605a46b14ff5e545b3c2b6dc0844b52
Kevin Feng <kevinfeng.cs88@gmail.com>
2022-03-10 12:05:26 -0500

    added API for fetching chats

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
-app.use('/api/chat/message', messageRoutes);
+app.use('/api/message', messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
