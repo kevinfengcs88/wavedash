0b9a7b463bc7bba90ca8e820bc3085ca8af7d130
Kevin Feng <kevinfeng.cs88@gmail.com>
2022-03-09 21:53:15 -0500

    added initial chat box UI

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,5 +1,5 @@
 import { Box } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import ChatBox from '../components/ChatBox';
 import SideDrawer from '../components/miscellaneous/SideDrawer';
 import MyChats from '../components/MyChats';
@@ -7,6 +7,7 @@ import { ChatState } from '../Context/ChatProvider';
 
 const ChatPage = () => {
   const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
   
   return (
     <div style={{ width:'100%' }}>
@@ -18,8 +19,8 @@ const ChatPage = () => {
         height='91.5vh'
         p='10px'
       >
-        {user && <MyChats />}
-        {user && <ChatBox />}
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />}
       </Box>
     </div>
   )
