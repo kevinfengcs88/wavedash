e07b9bcfb0591b526247da81faef10348b27aa2d
Kevin Feng <kevinfeng.cs88@gmail.com>
2022-03-14 17:07:18 -0400

    fixed console error about uncontrolled inputs on login page

--- frontend/src/components/authentication/Login.js
+++ frontend/src/components/authentication/Login.js
@@ -63,7 +63,7 @@ const Login = () => {
                 <FormLabel>Email</FormLabel>
                 <Input 
                     placeholder='Enter your email'
-                    value={email}
+                    value={email || ''}
                     onChange={(e)=>setEmail(e.target.value)}
                 />
             </FormControl>
@@ -73,7 +73,7 @@ const Login = () => {
                     <Input 
                         type={show ? 'text' : 'password'}
                         placeholder='Enter your password'
-                        value={password}
+                        value={password || ''}
                         onChange={(e)=>setPassword(e.target.value)}
                     />
                     <InputRightElement width='4.5rem'>
